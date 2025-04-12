b2f8b589f20f07d9afda9bae2abc05b8ed0e74d5
malhussein-bit <alhussein.m65@gmail.com>
2025-04-11 20:19:23 -0400

    Delete Classic-Models-Dashboard-Project/MySQL_Query/project_query.sql directory
    the files were wrong

--- Classic-Models-Dashboard-Project/MySQL_Query/project_query.sql/runing_total_payments.sql
+++
@@ -1,6 +0,0 @@
-/* customers that have gone over their credit limit factoring in their runing total and payments made- part 2 */
-/* we need to dreate a runing total for payments as well*/
-
-select *, sum(amount) over (partition by customerNumber order by paymentDate) as runing_total_payments
-from payments
-where customerNumber = 103
